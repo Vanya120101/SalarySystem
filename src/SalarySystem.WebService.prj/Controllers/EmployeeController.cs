035b4291df37a9fa9a1a8ee5ac80a56650616c5f
Vanya <Vanya.120101@yandex.ru>
2022-07-26 23:12:00 +0300

    Added Employees page

---
+++ src/SalarySystem.WebService.prj/Controllers/EmployeeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalarySystem.WebService.Controllers;
+
+public class EmployeeController : Controller
+{
+	public IActionResult Employees()
+	{
+		return View();
+	}
+}
--- src/SalarySystem.WebService.prj/Startup.cs
+++ src/SalarySystem.WebService.prj/Startup.cs
@@ -12,6 +12,7 @@ public class Startup
 	// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 	public void ConfigureServices(IServiceCollection services)
 	{
+		services.AddMvc();
 	}
 
 	// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -21,9 +22,15 @@ public class Startup
 		{
 			app.UseDeveloperExceptionPage();
 		}
-
+		app.UseStaticFiles();
 		app.UseRouting();
 
-		app.UseEndpoints(endpoints => endpoints.MapGet("/", async context => await context.Response.WriteAsync("Hello World!")));
+		app.UseEndpoints(endpoints =>
+		{
+			endpoints.MapControllerRoute(
+				name: "default",
+				pattern: "{controller}/{action}");
+		});
+		//app.UseEndpoints(endpoints => endpoints.MapGet("/", async context => await context.Response.WriteAsync("Hello World!")));
 	}
 }
